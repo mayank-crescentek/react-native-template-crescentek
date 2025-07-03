f7c2d41f1ab4f71ddf527f5fd5cbcf6f11853c57
mayank <mayank.balar@crescentek.com>
2025-07-03 14:59:52 +0530

    initial

---
+++ src/foundation/colors/color.js
@@ -0,0 +1,7 @@
+export const colors = {
+  black: '#000',
+  white: '#FFF',
+  link: '#4290F5',
+  red: 'red',
+  green: 'green',
+};
---
+++ src/foundation/textVariants/textVariants.js
@@ -0,0 +1,24 @@
+export const textVariants = {
+  heading: {
+    fontFamily: 'Poppins',
+    fontWeight: '900',
+    fontSize: 22,
+    lineHeight: 40,
+    letterSpacing: 0.3,
+    color: 'black',
+  },
+  heading1: {
+    fontFamily: 'Poppins',
+    fontWeight: '900',
+    fontSize: 18,
+    lineHeight: 40,
+    letterSpacing: 0.3,
+    color: 'red',
+  },
+  body: {
+    fontFamily: 'Poppins',
+    fontWeight: '500',
+    fontSize: 16,
+    color: 'black',
+  },
+};
