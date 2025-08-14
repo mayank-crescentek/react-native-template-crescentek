006c3abdde5de918ce18fd5a47b37b213edcee41
mayank <mayank.balar@crescentek.com>
2025-08-14 16:26:55 +0530

    updated ReadME

--- template/src/App.js
+++ template/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, View } from 'react-native';
-import Text1 from './components/common/Text';
+import Text from './components/common/Text';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './foundation/theme/theme';
 
 export default function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <StatusBar />
         <View>
-          <Text1 variant={'heading1'} style={{ marginVertical: 7, marginHorizontal: 10 }}>
+          <Text variant={'heading1'} style={{ marginVertical: 7, marginHorizontal: 10 }}>
             Template
-          </Text1>
+          </Text>
         </View>
       </SafeAreaView>
     </ThemeProvider>
